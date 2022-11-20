08c127225ad9fd5a891e85bd312f3872605acef0
codenor <95989314+codenor@users.noreply.github.com>
2022-11-20 18:32:42 +1100

    🎨 Format code

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -5,19 +5,13 @@ function Navbar() {
   return (
     <ul>
       <li>
-        <Link href="/">
-          Home
-        </Link>
+        <Link href="/">Home</Link>
       </li>
       <li>
-        <Link href="/about">
-          About
-        </Link>
+        <Link href="/about">About</Link>
       </li>
       <li>
-        <Link href="/projects">
-          Projects
-        </Link>
+        <Link href="/projects">Projects</Link>
       </li>
     </ul>
   );
