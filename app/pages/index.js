9d8b5ff2156f23fa3874e32692a2385dd524dd2f
codenor <95989314+codenor@users.noreply.github.com>
2022-11-22 19:18:40 +1100

    ♻️ Move navbar to _app.js

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,12 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <Head>
         <title>Arbnor | arbnor.xyz</title>
         <meta name="description" content="name and domain name" />
