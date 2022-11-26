23d5ec471cd105a2e43053313ca937addd093f9b
codenor <95989314+codenor@users.noreply.github.com>
2022-11-26 20:18:23 +1100

    ✨ Add custom 404 page

---
+++ app/pages/404.js
@@ -0,0 +1,10 @@
+import { FaSadCry } from 'react-icons/fa';
+
+export default function Custom404() {
+  return (
+    <div className="error">
+    <h1>☹</h1>
+    <h2>404 - Page Not Found </h2>
+    </div>
+  );
+}
