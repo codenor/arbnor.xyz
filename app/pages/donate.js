06facc45eedbfc1045494f12ef97b0e5395092cc
Arbnor <95989314+codenor@users.noreply.github.com>
2022-12-26 19:06:41 +1100

    💸 Update monero wallet address

--- app/pages/donate.js
+++ app/pages/donate.js
@@ -9,7 +9,7 @@ export default function donations() {
         <p>If you want to support me you can donate through the following:</p>
         <h2>Crypto</h2>
         <p>Currently I only accept Monero (XMR):</p> 
-        <p2>4AaMkD5aqsKZCCEJWZ5mMpNDToo4bMCee8jjDbPTmpM2ioLGwTgQbiwYULQEyoJGj1iTzPTe8zjZT1ZTGEQiaEFU41dpzwL</p2>
+        <p2>456oUvDh4ndGarLt29CRGM9CjSfPA5E4F4Lyiia38oGCgsVzK62zgUq4FsvRQWDuFEi4cRNZTZyQTJTJ2QcCquZG8o2BPEU</p2>
       </div>
     </div>
   );
